5a43512067fe8facf6b5a03fd7777b4e24684846
moshiurrahmansourave <moshiurrahmansourave@gmail.com>
2023-09-24 17:38:35 +0600

    add update

--- src/component/CatagoryList/CatagoryList.jsx
+++ src/component/CatagoryList/CatagoryList.jsx
@@ -3,7 +3,7 @@
 const CatagoryList = () => {
     return (
         <div className="text-center ">
-            <h2 className="text-5xl my-3 ">Job Catagory List</h2>
+            <h2 className="text-5xl my-3 ">Job Catagory List !!!!!</h2>
             <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
         </div>
     );
